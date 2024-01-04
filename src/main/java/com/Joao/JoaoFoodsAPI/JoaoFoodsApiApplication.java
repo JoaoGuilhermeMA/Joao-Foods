6587512759782520d693c4feadc274c9f908b5c0
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-01-04 17:02:23 -0300

    criando o projeto

---
+++ src/main/java/com/Joao/JoaoFoodsAPI/JoaoFoodsApiApplication.java
@@ -0,0 +1,13 @@
+package com.Joao.JoaoFoodsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaoFoodsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaoFoodsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Joao/JoaoFoodsAPI/JoaoFoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Joao.JoaoFoodsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaoFoodsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

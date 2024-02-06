3c834aa0560d1fca931d301acc92bb508ee95e8a
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-02-06 13:59:21 -0300

    ponto de injeção, anotação autowired e dependencia opcional

--- src/main/java/com/Joao/JoaoFoodsAPI/di/service/AtivacaoClienteService.java
+++ src/main/java/com/Joao/JoaoFoodsAPI/di/service/AtivacaoClienteService.java
@@ -2,21 +2,29 @@ package com.Joao.JoaoFoodsAPI.di.service;
 
 import com.Joao.JoaoFoodsAPI.di.modelo.Cliente;
 import com.Joao.JoaoFoodsAPI.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
+
+    @Autowired(required = false)
     private Notificador notificador;
 
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
-
-        System.out.println("AtivacaoClienteService: " + notificador);
     }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        if (notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        }else {
+            System.out.println("Não existe notificador, mas cliente foi ativado");
+        }
+
+
     }
+
 }
